76bc8a1be79c03df0d248fac847772f5ae15c2f1
Kamil Nawrot <kamilnawrot98@gmail.com>
2020-06-20 09:22:53 +0200

    Updates

---
+++ component---src-pages-page-2-pl-js-38a99bf85def2c236a32.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"8X/P":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(e){var t=e.location;return l.a.createElement(c.a,{location:t,lang:"pl"},l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Druga strona"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/pl"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-pl-js-38a99bf85def2c236a32.js.map
