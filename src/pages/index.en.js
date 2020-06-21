d5049f47f699e08a5c43210ed3f98a2b4df8053f
Kamil Nawrot <kamilnawrot98@gmail.com>
2020-06-21 20:01:38 +0200

    finished static layout of the welcome section

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
-import BackgroundImage from 'gatsby-background-image'
+import BackgroundImage from "gatsby-background-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import '../styles/layout.scss'
+import "../styles/layout.scss"
 
 const HeroImage = () => {
   const data = useStaticQuery(graphql`
@@ -22,17 +22,38 @@ const HeroImage = () => {
   `)
 
   return (
-    <BackgroundImage Tag="section" className="hero-image" fluid={data.heroImage.childImageSharp.fluid}>
+    <BackgroundImage
+      Tag="section"
+      className="hero-image"
+      fluid={data.heroImage.childImageSharp.fluid}
+    >
       <div className="hero-image__content">
-        <h1 className="hero-image__content__headline">Lorem ipsum dolor sit amet</h1>
-        <button className="hero-image__content__button">Quesi varites semiratiu</button>
-        <p className="hero-image__content__paragraph">Eu et eu veniam eu velit incididunt minim culpa incididunt ad id quis consequat amet. Lorem consectetur consequat mollit quis do eiusmod enim consequat. Nostrud voluptate nostrud eiusmod mollit Lorem minim eu voluptate aute aliqua qui tempor. Cupidatat amet voluptate adipisicing mollit incididunt deserunt proident. In laborum laborum irure consequat consequat officia ex excepteur cillum eu eiusmod ex mollit sint. Veniam ea ullamco deserunt id in aute laborum labore anim culpa nulla.</p>
+        <div class="hero-image__content__element-wrapper">
+          <h1 className="hero-image__content__element hero-image__content__headline">
+            Lorem ipsum dolor sit amet
+          </h1>
+        </div>
+        <div class="hero-image__content__element-wrapper">
+          <button className="hero-image__content__element hero-image__content__button">
+            Quesi varites semiratiu
+          </button>
+        </div>
+        <p className="hero-image__content__paragraph">
+          Eu et eu veniam eu velit incididunt minim culpa incididunt ad id quis
+          consequat amet. Lorem consectetur consequat mollit quis do eiusmod
+          enim consequat. Nostrud voluptate nostrud eiusmod mollit Lorem minim
+          eu voluptate aute aliqua qui tempor. Cupidatat amet voluptate
+          adipisicing mollit incididunt deserunt proident. In laborum laborum
+          irure consequat consequat officia ex excepteur cillum eu eiusmod ex
+          mollit sint. Veniam ea ullamco deserunt id in aute laborum labore anim
+          culpa nulla.
+        </p>
       </div>
     </BackgroundImage>
-  );
+  )
 }
 
-const IndexPage = ({location}) => (
+const IndexPage = ({ location }) => (
   <Layout location={location} lang="en">
     <SEO title="Home" />
     <HeroImage />
