e7e340cf2070bac500ffea09f09afbd2188aa71b
Kamil Nawrot <kamilnawrot98@gmail.com>
2020-06-22 00:20:02 +0200

    implemented personal traits display

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,13 +30,7 @@ const Layout = ({ location, lang, children }) => {
   return (
     <>
       <Header location={location} lang={lang} menuItems={data.site.siteMetadata.menuItems} languages={data.site.siteMetadata.languages} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -3,11 +3,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import HeroImage from '../components/hero-image'
+import AboutSection from '../components/about-section'
 
 const IndexPage = ({ location }) => (
   <Layout location={location} lang="en">
     <SEO title="Home" />
     <HeroImage />
+    <AboutSection />
   </Layout>
 )
 
