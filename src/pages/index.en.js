72eea10dc131a3cc417031adfb9bb980b642920e
Kamil Nawrot <kamilnawrot98@gmail.com>
2020-06-23 10:32:27 +0200

    resolve minor conflicts

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -2,16 +2,6 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-<<<<<<< HEAD
-import HeroImage from '../components/hero-image'
-import AboutSection from '../components/about-section'
-
-const IndexPage = ({ location }) => (
-  <Layout location={location} lang="en">
-    <SEO title="Home" />
-    <HeroImage />
-    <AboutSection />
-=======
 import HeroImage from "../components/hero-image"
 import AboutSection from "../components/about-section"
 import ProjectsSection from "../components/projects-section"
@@ -22,7 +12,7 @@ const IndexPage = ({ location }) => (
     <HeroImage/>
     <AboutSection/>
     <ProjectsSection/>
->>>>>>> fdeaac5dab8bf8ed3cf021e09bcbc875d83b8067
   </Layout>
+)
 
 export default IndexPage
