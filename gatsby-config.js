ebf0c17c979fb038daf8a98d1bf66bb3f2e7cfd2
Kamil Nawrot <kamilnawrot98@gmail.com>
2020-06-24 08:41:10 +0200

    added SVG icons, created component for displaying them

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        svgo: false,
+        icon: true,
+        rule: {
+          include: /\.icon\.svg$/
+        }
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
