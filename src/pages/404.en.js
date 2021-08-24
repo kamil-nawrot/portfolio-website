9c89ac72a6efd94574035a61efaffdc8f637b73c
kamil-nawrot <kamilnawrot98@gmail.com>
2021-08-24 20:32:47 +0200

    implemented initial 404 page design

--- src/pages/404.en.js
+++ src/pages/404.en.js
@@ -27,10 +27,20 @@ const NotFoundPage = ({ location }) => {
         footer={dataEn.footer}>
             <BackgroundImage
                 Tag="section"
-                className="not-found-image"
+                className="not-found-section"
                 fluid={image.notFoundImage.childImageSharp.fluid}
             >
-                <p>dupa</p>
+                <div className="not-found-section__content">
+                    <h1 className="not-found-section__content__headline">
+                        Unfortunately, there's nothing here.
+                    </h1>
+                    <p className="not-found-section__content__paragraph">
+                        But don't worry, here's something that'll bring you back on track:
+                    </p>
+                    <button className="not-found-section__content__button">
+                        &lt; Back to homepage
+                    </button>
+                </div>
             </BackgroundImage>
         </Layout>
     )
