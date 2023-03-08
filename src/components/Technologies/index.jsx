d87e94a38bdaca54bf0c04d425a7296f7df39c78
Kamil Nawrot <39823440+kamil-nawrot@users.noreply.github.com>
2023-03-08 09:42:48 +0100

    PW-46 added new SVG icons
    * PW-46 added new SVG icons
    
    * PW-46 changed icons displayed by Technologies component

--- src/components/Technologies/index.jsx
+++ src/components/Technologies/index.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 
-import ReactIcon from '../../images/tech-icons/react.icon.svg'
-import ReduxIcon from '../../images/tech-icons/redux.icon.svg'
 import JsIcon from '../../images/tech-icons/javascript.icon.svg'
 import CssIcon from '../../images/tech-icons/css5.icon.svg'
 import HtmlIcon from '../../images/tech-icons/html5.icon.svg'
 import NodeIcon from '../../images/tech-icons/nodejs.icon.svg'
-import WordpressIcon from '../../images/tech-icons/wordpress.icon.svg'
+import AngularIcon from '../../images/tech-icons/angular.icon.svg'
+import RxjsIcon from '../../images/tech-icons/rxjs.icon.svg'
+import GraphqlIcon from '../../images/tech-icons/graphql.icon.svg'
 
 import './technologies.scss'
 
 const Technologies = () => (
   <section className="technologies">
-    <ReactIcon className="technologies__icon" />
-    <ReduxIcon className="technologies__icon" />
+    <AngularIcon className="technologies__icon" />
+    <RxjsIcon className="technologies__icon" />
     <HtmlIcon className="technologies__icon" />
     <CssIcon className="technologies__icon" />
     <JsIcon className="technologies__icon" />
-    <WordpressIcon className="technologies__icon" />
+    <GraphqlIcon className="technologies__icon" />
     <NodeIcon className="technologies__icon" />
   </section>
 )
